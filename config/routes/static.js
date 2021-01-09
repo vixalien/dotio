fda2cdbfe997cb9c916a1dcfd69c355786bb58f6
Angelo Shema <geoangercola@gmail.com>
2021-01-09 18:25:42 +0200

    Use different bundles according to environment

--- config/routes/static.js
+++ config/routes/static.js
@@ -2,18 +2,20 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 
+let libName = (lib) => process.env.NODE_ENV == "development" ? `${lib}/umd/${lib}.production.min.js` : `${lib}/umd/${lib}.development.js`
+
 export default (app) => {
 	// React and his sister
 	app.use('/lib/react.js', (req, res) => {
     res.setHeader('Content-Type', 'application/javascript; charset=UTF-8')
 		res.status(200);
-		res.send(fs.readFileSync(require.resolve('react/umd/react.production.min.js')).toString());
+		res.send(fs.readFileSync(require.resolve(libName('react'))).toString());
 	});
 
 	app.use('/lib/react-dom.js', (req, res) => {
     res.setHeader('Content-Type', 'application/javascript; charset=UTF-8')
 		res.status(200)
-		res.send(fs.readFileSync(require.resolve('react-dom/umd/react-dom.production.min.js')).toString());
+		res.send(fs.readFileSync(require.resolve(libName('react-dom'))).toString());
 	});
 
 	// Public
