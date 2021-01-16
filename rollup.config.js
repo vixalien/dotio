1a9201be2bdc9ee19cbf9eda51a915cfcdddd874
Angelo Shema <geoangercola@gmail.com>
2021-01-16 22:57:36 +0200

    Compile to .build dir, as before

--- rollup.config.js
+++ rollup.config.js
@@ -94,7 +94,7 @@ export default [
 		input: './index.js',
 		output: [
 			{
-				file: 'api/server/index.js',
+				file: '.build/server/index.js',
 				format: 'cjs',
 				sourcemap: 'inline',
 				sourcemapExcludeSources: true
