aa5666f8df3ed567df1cdfadab50314aaa7813c4
Angelo Shema <geoangercola@gmail.com>
2021-01-19 01:37:11 +0200

    Post was using blog_id, while it had been renamed to post_id, fixed it

--- controllers/post.js
+++ controllers/post.js
@@ -7,7 +7,7 @@ var db = require('../db');
 exports.engine = 'md';
 
 exports.show = function(req, res, next){
-	var blog = req.params.blog_id;
+	var blog = req.params.post_id;
   if (!blog) return next('route');
   res.render('../../blog/hello', { blog });
 };
