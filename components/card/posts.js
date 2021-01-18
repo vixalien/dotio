84d69474e72932f5c35457d8d2c78af5c30e8d76
Angelo Shema <geoangercola@gmail.com>
2021-01-19 01:23:11 +0200

    Now use actual posts, not random data

--- components/card/posts.js
+++ components/card/posts.js
@@ -1,40 +1,30 @@
 import Card from './index';
 import Line from './line';
 
-let text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+import postsJSON from '.build/data/posts.json'
 
-let random = () => {
-	let arr = text.split(' ');
-	return arr.splice(0, Math.floor(Math.random() * (arr.length - 1))).join(' ')
+let posts = Object.entries(postsJSON)
+	.filter((_, id) => id < 4)
+
+let Posts = (props) => {
+	return posts.map(([slug, {title, description, created}]) => <Line
+		title={title}
+		date={new Date(created).toDateString()}
+		text={description}
+		href={"/post/"+slug}
+	/>)
 }
 
-let SamplePost = (props) => {
-	return <Line
-		title="Sample Post"
-		alt="Sample Post"
-		href="/post/random"
-		{...props}
-	/>
-}
-
-let ProjectsCard = () => {
+let PostsCard = () => {
 	return <Card
-		title="Posts"
+		title="Recent Posts"
 		link="View All"
 		href="/posts"
 		column
 		hr={true}
 	>
-		<SamplePost text={random()}/>
-		<SamplePost text={random()}/>
-		<SamplePost text={random()}/>
-		<SamplePost text={random()}/>
+		<Posts/>
 	</Card>
 }
 
-export default ProjectsCard;
+export default PostsCard;
