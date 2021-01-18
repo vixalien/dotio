3c8011d26e62d929d13627974ecb113c130ee5b9
Angelo Shema <geoangercola@gmail.com>
2021-01-18 18:55:27 +0200

    Re-add hydrate paths and clean things up

--- config/views/react.js
+++ config/views/react.js
@@ -6,28 +6,32 @@ const React = require('react');
 const { renderToString } = require('react-dom/server');
 import { flushToHTML } from 'styled-jsx/server'
 
-let buildPath = path.join(process.cwd(), '.build');
-let templatePath = path.join(buildPath, 'lib', 'template.html');
-let wrapperPath = path.join(buildPath, 'lib', 'wrapper.js');
-let hydratePath = path.join(buildPath, 'lib', 'hydrate.js');
+let resolve = (...args) => path.join(process.cwd(), '.build', ...args)
+let read = (...args) => fs.readFileSync(...args).toString();
+let deploy_url = () => {
+	let deploy_url = process.env.VERCEL_URL || process.env.URL || 'vixaliendotio.vercel.app';
+	deploy_url.startsWith('localhost') ? deploy_url = "http://" + deploy_url : deploy_url = "https://" + deploy_url
+	return deploy_url;
+}
+
+let templatePath = resolve('lib', 'template.html');
+let wrapperPath = resolve('lib', 'wrapper.js');
+let hydratePath = resolve('lib', 'hydrate.js');
 
 export default async (filePath, options, callback) => { // define the template engine
 	try {
 		// Load required files
-		let template = fs.readFileSync(templatePath).toString();
+		let template = read(templatePath);
 		let Wrapper = require(wrapperPath);
-		let hydrate = fs.readFileSync(hydratePath).toString();	
+		let hydrate = read(hydratePath);	
 
 		// Load the file
 		let src = path.relative(process.cwd(), filePath);
-		let buildSrc = path.join(buildPath, 'views', src);
+		let buildSrc = resolve('views', src);
 		let Content = await import(buildSrc).then(e => e.default);
 
 		// Build props
 		let props = options;
-
-		let deploy_url = process.env.VERCEL_URL || process.env.URL || 'vixaliendotio.vercel.app';
-		deploy_url.startsWith('localhost') ? deploy_url = "http://" + deploy_url : deploy_url = "https://" + deploy_url
 
 		// Build the file
 		let rendered = renderToString(Wrapper(Content, props));
