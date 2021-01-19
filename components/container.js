004d49b411e8673100aa1b24ad18fecf366c37e2
Angelo Shema <geoangercola@gmail.com>
2021-01-19 16:25:49 +0200

    Dynamic container sizes

--- components/container.js
+++ components/container.js
@@ -1,14 +1,21 @@
 let Container = ({ tb = true, rl = true, t = true, b = true, ...props }) => {
 	if (!tb) { t = false; b = false; };
-	let paddingTop    = (t  ? 'var(--padding-top)   ' : '0');
-	let paddingBottom = (b  ? 'var(--padding-bottom)' : '0');
-	let paddingRight  = (rl ? 'var(--padding-right) ' : '0');
-	let paddingLeft   = (rl ? 'var(--padding-left)  ' : '0');
+	let pad = (param, variable) => {
+		if (param) {
+			if (parseInt(param)) {
+				return parseInt(param) + 'px';
+			} else {
+				return 'var(--padding-' + variable + ');';
+			}
+		} else {
+			return '0';
+		}
+	}
 	return <>
 		<div {...props}/>
 		<style jsx>{`
 			div {
-				padding: ${paddingTop} ${paddingRight} ${paddingBottom} ${paddingLeft};
+				padding: ${pad(t, top)} ${pad(r, right)} ${pad(b, bottom)} ${pad(l, left)};
 				max-width: var(--max-width);
 				width: 100%;
 				margin: auto;
